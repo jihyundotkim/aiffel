ab363020d8ae81e1ae29d7abe59ebe806a21b061
Jihyun <contact@jihyun.kim>
2021-09-24 07:16:11 +0900

    Finished the model.

--- 2021-09-23/rock_scissors_papers.py
+++ 2021-09-23/rock_scissors_papers.py
@@ -1,7 +1,12 @@
 from PIL import Image
+import glob
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.optimizers import Adam
 import os
+
+tf.random.set_seed(42)
 
 def resize_images(img_path):
 	images=glob.glob(img_path + "/*.jpg")  
@@ -16,15 +21,14 @@ def resize_images(img_path):
 		new_img.save(img, "JPEG")
     
 	print(len(images), " images resized.")
-	
-# 가위 이미지가 저장된 디렉토리 아래의 모든 jpg 파일을 읽어들여서
-image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/scissor"
-resize_images(image_dir_path)
-image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/rock"
-resize_images(image_dir_path)
-image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/paper"
-resize_images(image_dir_path)
-print("이미지 resize 완료!")
+
+# image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/training/scissor"
+# resize_images(image_dir_path)
+# image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/training/rock"
+# resize_images(image_dir_path)
+# image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/training/paper"
+# resize_images(image_dir_path)
+# print("이미지 resize 완료!")
 
 def load_data(img_path, number_of_data=300):  # 가위바위보 이미지 개수 총합에 주의하세요.
     # 가위 : 0, 바위 : 1, 보 : 2
@@ -52,71 +56,49 @@ def load_data(img_path, number_of_data=300):  # 가위바위보 이미지 개수
         imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
         labels[idx]=2   # 보 : 2
         idx=idx+1
-        
-    print("학습데이터(x_train)의 이미지 개수는", idx,"입니다.")
+
+    print("이미지 수:", idx)
     return imgs, labels
 
 image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23"
 (x_train, y_train)=load_data(image_dir_path)
-x_train_norm = x_train/255.0   # 입력은 0~1 사이의 값으로 정규화
+x_train_norm = x_train/255.0
+
 
 print("x_train shape: {}".format(x_train.shape))
 print("y_train shape: {}".format(y_train.shape))
 
 model=keras.models.Sequential()
-model.add(keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(28,28,3)))
+model.add(keras.layers.Conv2D(100, (3,3), activation='relu', input_shape=(28,28,3)))
 model.add(keras.layers.MaxPool2D(2,2))
-model.add(keras.layers.Conv2D(32, (3,3), activation='relu'))
+model.add(keras.layers.Conv2D(300, (3,3), activation='relu'))
 model.add(keras.layers.MaxPooling2D((2,2)))
 model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(32, activation='relu'))
-model.add(keras.layers.Dense(10, activation='softmax'))
+model.add(keras.layers.Dense(1000, activation='relu'))
+model.add(keras.layers.Dense(3, activation='softmax'))
 
 print('Model에 추가된 Layer 개수: ', len(model.layers))
 
 model.summary()
 
-model.compile(optimizer='adam',
+model.compile(optimizer=Adam(learning_rate=0.0001),
              loss='sparse_categorical_crossentropy',
              metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=10)
+model.fit(x_train_norm, y_train, epochs=10)
 
-def load_test_data(img_path, number_of_data=300):  # 가위바위보 이미지 개수 총합에 주의하세요.
-    # 가위 : 0, 바위 : 1, 보 : 2
-    img_size=28
-    color=3
-    #이미지 데이터와 라벨(가위 : 0, 바위 : 1, 보 : 2) 데이터를 담을 행렬(matrix) 영역을 생성합니다.
-    imgs=np.zeros(number_of_data*img_size*img_size*color,dtype=np.int32).reshape(number_of_data,img_size,img_size,color)
-    labels=np.zeros(number_of_data,dtype=np.int32)
+# image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/test/scissor"
+# resize_images(image_dir_path)
+# image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/test/rock"
+# resize_images(image_dir_path)
+# image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/test/paper"
+# resize_images(image_dir_path)
 
-    idx=0
-    for file in glob.iglob(img_path+'/scissor/test/*.jpg'):
-        img = np.array(Image.open(file),dtype=np.int32)
-        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
-        labels[idx]=0   # 가위 : 0
-        idx=idx+1
+image_dir_path = os.getenv("HOME") + "/AIFFEL/2021-09-23/test"
+(x_test, y_test)=load_data(image_dir_path)
+x_test_norm = x_test/255.0   # 입력은 0~1 사이로 정규화
 
-    for file in glob.iglob(img_path+'/rock/test/*.jpg'):
-        img = np.array(Image.open(file),dtype=np.int32)
-        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
-        labels[idx]=1   # 바위 : 1
-        idx=idx+1  
-    
-    for file in glob.iglob(img_path+'/paper/test/*.jpg'):
-        img = np.array(Image.open(file),dtype=np.int32)
-        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
-        labels[idx]=2   # 보 : 2
-        idx=idx+1
-        
-    print("학습데이터(x_train)의 이미지 개수는", idx,"입니다.")
-    return imgs, labels
+model.evaluate(x_test_norm, y_test, batch_size=10)
 
-image_dir_path = os.getenv("HOME") + "/aiffel/rock_scissor_paper"
-(x_test, y_test=load_test_data(image_dir_path)
-x_test_norm = x_test/255.0   # 입력은 0~1 사이의 값으로 정규화
-
-print("x_train shape: {}".format(x_test.shape))
-print("y_train shape: {}".format(y_test.shape))
-
-model.evaluate(x_test, y_test, epochs=10)
+predicted_result = model.predict(x_test_norm)  # model이 추론한 확률. 
+predicted_labels = np.argmax(predicted_result, axis=1)
